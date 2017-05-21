348d3968266881c3a08d30b823d67dcfa078007f
marius.alba <kleemannmarius@gmail.com>
2017-05-21 21:12:12 +0200

    c

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 });
 
 document.addEventListener('DOMContentLoaded', function(){
-      Typed.new('.title' '.titleAbout', {
+      Typed.new('.title','.titleAbout', {
         strings: ["Always Learning Always Evolving"],
         typeSpeed: 50
       });
