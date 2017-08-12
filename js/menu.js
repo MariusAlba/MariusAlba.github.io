7ea3133f3003e7de6864edbc8a1b96142b697f21
marius.alba <kleemannmarius@gmail.com>
2017-08-12 18:55:40 +0200

    Added a back to top arrow on the right and alterd the typed.js so it will type another and also loop

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,37 @@ $(document).ready(function(){
         $("nav ul").toggleClass("showIcon");
     });
 });
+$(document).ready(function() {
+   var btt = $('.bTop');
+    
+    btt.on('click', function(e) {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+        e.preventDefault();
+    });
+    
+    $(window).on('scroll', function(){
+       var bt = $(this),
+           height = bt.height(),
+           top = bt.scrollTop();
+        if(top > height) {
+            if(!btt.is(':visible')){
+                btt.fadeIn();
+            }
+        }
+        else {
+            btt.fadeOut();
+        }
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function(){
       Typed.new('.title', {
-        strings: ["Always Learning Always Evolving"],
-        typeSpeed: 50
+        strings: ["Always Learning Always Evolving","Technology is a way to adapt"],
+        typeSpeed: 70,
+        backDelay: 2000,
+        loop: true
       });
   });
 
